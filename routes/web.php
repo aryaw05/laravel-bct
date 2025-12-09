30a222be1a5e90cf52e19aec6ad0fdb0c5bac232
aryaw05 <aryawahyuaw77@gmail.com>
2025-12-09 19:16:41 +0700

    feat: update database name and add student routes
    - Change DB_DATABASE from 'siswa' to 'student_management_system'
    - Add resource route for StudentController
    - Minor formatting improvements in Controller class

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
 
+
+Route::resource('students', StudentController::class);
 Route::get('/', function () {
     return view('welcome');
 });
